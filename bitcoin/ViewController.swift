d9330b88403a636d19e811d61f3a45faca7ff2be
Eddy Segura <eseguran@outlook.com>
2018-04-27 20:37:13 -0600

    initial

--- bitcoin/ViewController.swift
+++ bitcoin/ViewController.swift
@@ -9,7 +9,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    let precio = 15;
+    var cash = 0;
+    var cashback = 0;
+    var cantidad = 0;
+    var bought = 0;
+    
+    @IBOutlet weak var lblcompra: UILabel!
+    
+    @IBOutlet weak var lblcash: UILabel!
+    @IBOutlet weak var txtcash: UITextField!
+    
+    func compra (){
+        cash = Int(txtcash.text!)!;
+        if(cash/precio>0){
+            cantidad = cash/precio;
+           
+        }
+        
+        
+    }
+    
+    func change (){
+        cashback = (cash%precio)
+        
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +46,14 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func comprar(_ sender: Any) {
+        
+        compra();
+        change();
+        lblcompra.text = String(cantidad);
+        lblcash.text = String(cashback);
+        
+    }
+    
 }
 
